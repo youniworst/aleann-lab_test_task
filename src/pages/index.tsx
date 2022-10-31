2ac89083ab12a6b83f03d322bcd48c3fe6f0d08d
youniworst <oleg13kozak2207@gmail.com>
2022-10-31 14:48:55 +0200

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import styles from "./Home.module.scss";
+
+const Home: NextPage = () => {
+  return <main className={styles.main}></main>;
+};
+
+export default Home;
